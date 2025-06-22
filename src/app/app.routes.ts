00a5f80d1bd44282712b45b10002f2d6fd093016
gabrielmdss <gersongabriel_@hotmail.com>
2025-06-21 22:13:18 -0300

    refactor: ajusta bibliotecas do httpProvider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'lista',
     pathMatch: 'full',
   },
   {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
-  },
-  {
     path: 'lista',
-    loadComponent: () => import('./paginas/lista/lista.page').then( m => m.ListaPage)
-  },
+    loadComponent: () =>
+      import('./paginas/lista/lista.page').then((m) => m.ListaPage),
+  }
 ];
